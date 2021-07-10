d9fbe4c85fb1097fcd5081ed4af506ec55926d2c
Mateusz <mateuszpenkala@gmail.com>
2021-07-10 21:51:19 +0200

    Add Footer.js and Navbar.js

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer className="footer">
+            <p className="footer__copyright"> &copy; {new Date().getFullYear()} by Mateusz Penkala</p>
+        </footer>
+     );
+}
+ 
+export default Footer;
