9a6d0efac53d904a123a3565031d6b298c3ba0fc
Mateusz Penkala <mateuszpenkala@gmail.com>
2021-08-09 18:18:48 +0200

    Doanie komponentu FuelPrices

---
+++ src/components/FuelPrices.js
@@ -0,0 +1,57 @@
+const FuelPrices = () => {
+    const fuelPrices = [
+        {
+            id: 1,
+            name: "95",
+            price: 5.72,
+            currency: 'zł'
+        },
+        {
+            id: 2,
+            name: "98",
+            price: 5.97,
+            currency: 'zł'
+        },
+        {
+            id: 3,
+            name: "ON",
+            price: 5.44,
+            currency: 'zł'
+        },
+        {
+            id: 4,
+            name: "ON+",
+            price: 5.63,
+            currency: 'zł'
+        },
+        {
+            id: 5,
+            name: "LPG",
+            price: 2.64,
+            currency: 'zł'
+        }
+    ];
+
+    return ( 
+        <div className="fuel-prices">
+            <h2>Aktualne ceny paliw</h2>
+            <div className="fuel-prices__prices">
+                {fuelPrices.map(({id, name, price, currency, color}) => {
+                   return <Price name={name} price={price} currency={currency} color={color} key={id}/>
+                })}
+            </div>
+        </div>
+     );
+}
+
+const Price = ({name, price, currency, color}) => {
+    return (
+        <div className="price" style={{color: color}} onClick={() => {navigator.clipboard.writeText(price)}}>
+            <span className='price__fuel-name'>{name}</span>
+            <span className="price__amount">{price + ' ' + currency}</span>
+            {/* <span className="price__currency">zł</span> */}
+        </div>
+    )
+}
+ 
+export default FuelPrices;
